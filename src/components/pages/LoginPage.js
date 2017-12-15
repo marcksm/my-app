99ff2c06a21bd09e9916626155cb7d65b6992e22
Marcos Vinicius do Carmo Sousa <marcos.vinicius.wz@gmail.com>
2017-12-15 02:07:19 -0200

    login forms

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,10 @@
 import React from 'react'
 //import { Link } from 'react-router-dom'
-//import LoginForm from '../forms/LoginForm'
+import LoginForm from '../forms/LoginForm'
 const LoginPage = () => (
   <div>
     <h1>Login Page</h1>
-
+    <LoginForm/>
   </div>
 )
 
